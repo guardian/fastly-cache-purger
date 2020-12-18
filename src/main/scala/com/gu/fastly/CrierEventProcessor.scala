ecdee3170e3b862ac421385d84a1abea91592b85
Tony McCrae <tonytw1@users.noreply.github.com>
2020-12-18 15:10:26 +0000

    Duplicate batches (#31)
    * Quick test for duplicate content ids in this batch.
    
    Co-authored-by: Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>

--- src/main/scala/com/gu/fastly/CrierEventProcessor.scala
+++ src/main/scala/com/gu/fastly/CrierEventProcessor.scala
@@ -1,13 +1,34 @@
 package com.gu.fastly
 
 import com.amazonaws.services.kinesis.model.Record
-import com.gu.crier.model.event.v1.Event
+import com.gu.crier.model.event.v1.{Event, EventType, ItemType}
 import com.gu.thrift.serializer.ThriftDeserializer
+
 import scala.util.Try
 
 object CrierEventProcessor {
 
   def process(records: Seq[Record])(purge: Event => Boolean) = {
+
+    // Quick check for duplicates
+    val crierEvents: Seq[Event] = records.flatMap { record =>
+      eventFromRecord(record).toOption
+    }
+    val updateContentIds = crierEvents.flatMap { event =>
+      event.payloadId
+      (event.itemType, event.eventType) match {
+        case (ItemType.Content, EventType.Update | EventType.RetrievableUpdate) =>
+          Some(event.payloadId)
+        case _ =>
+          None
+      }
+    }
+    val uniqueContentIds = updateContentIds.toSet
+    println("Batch contained " + updateContentIds.size + " content updates ids and " + uniqueContentIds.size + " unique contentIds")
+    if (updateContentIds.size != uniqueContentIds.size) {
+      println("Batch may contain duplicate content ids")
+    }
+
     val processingResults: Iterable[Boolean] = records.flatMap { record =>
       val event = eventFromRecord(record)
       event.map { e =>
