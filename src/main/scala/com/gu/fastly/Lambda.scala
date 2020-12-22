900d58a71ea75116e134f0ee3970df028e102fb2
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2020-12-22 14:30:40 +0000

    Wrap potential IOException from Facebook call.
    Timeout would fit under this.

--- src/main/scala/com/gu/fastly/Lambda.scala
+++ src/main/scala/com/gu/fastly/Lambda.scala
@@ -1,7 +1,7 @@
 package com.gu.fastly
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder
-import com.amazonaws.services.cloudwatch.model.{ Dimension, MetricDatum, PutMetricDataRequest, StandardUnit }
+import com.amazonaws.services.cloudwatch.model.{Dimension, MetricDatum, PutMetricDataRequest, StandardUnit}
 import com.amazonaws.services.kinesis.clientlibrary.types.UserRecord
 import com.amazonaws.services.kinesis.model.Record
 import com.amazonaws.services.lambda.runtime.events.KinesisEvent
@@ -12,6 +12,7 @@ import io.circe.parser._
 import okhttp3._
 import org.apache.commons.codec.digest.DigestUtils
 
+import java.io.IOException
 import scala.collection.JavaConverters._
 
 class Lambda {
@@ -196,42 +197,52 @@ class Lambda {
     if (contentIsInterestingToFacebookNewstab) {
       val scope = config.facebookNewsTabScope
 
-      // The POST endpoint with URL encoded parameters as per New Tab documentation
-      val indexArticle = new HttpUrl.Builder()
-        .scheme("https")
-        .host("graph.facebook.com")
-        .addQueryParameter("id", contentWebUrl)
-        .addQueryParameter("scopes", scope)
-        .addQueryParameter("access_token", config.facebookNewsTabAccessToken)
-        .addQueryParameter("scrape", "true")
-        .build();
+      try {
+        // The POST endpoint with URL encoded parameters as per New Tab documentation
+        val indexArticle = new HttpUrl.Builder()
+          .scheme("https")
+          .host("graph.facebook.com")
+          .addQueryParameter("id", contentWebUrl)
+          .addQueryParameter("scopes", scope)
+          .addQueryParameter("access_token", config.facebookNewsTabAccessToken)
+          .addQueryParameter("scrape", "true")
+          .build()
 
-      val request = new Request.Builder()
-        .url(indexArticle)
-        .post(EmptyJsonBody)
-        .build()
+        val request = new Request.Builder()
+          .url(indexArticle)
+          .post(EmptyJsonBody)
+          .build()
 
-      val response = httpClient.newCall(request).execute()
+        val response = httpClient.newCall(request).execute()
 
-      // Soft evaluate the Facebook response
-      // Their documentation does not specifically mention response codes.
-      // Lets evaluate and log our interpretation of the response for now
-      val wasSuccessful = response.code match {
-        case 200 =>
-          decode[FacebookNewstabResponse](response.body.string()).fold({ error =>
-            println("Failed to parse Facebook Newstab response: " + error.getMessage)
+        // Soft evaluate the Facebook response
+        // Their documentation does not specifically mention response codes.
+        // Lets evaluate and log our interpretation of the response for now
+        val wasSuccessful = response.code match {
+          case 200 =>
+            decode[FacebookNewstabResponse](response.body.string()).fold({ error =>
+              println("Failed to parse Facebook Newstab response: " + error.getMessage)
+              false
+            }, { facebookResponse =>
+              facebookResponse.scopes.get(scope).contains("INDEXED")
+            })
+          case _ =>
+            println("Received unexpected response code from Facebook: " + _)
             false
-          }, { facebookResponse =>
-            facebookResponse.scopes.get(scope).contains("INDEXED")
-          })
-        case _ =>
-          println("Received unexpected response code from Facebook: " + _)
+        }
+
+        println(s"Sent Facebook Newstab ping request for content with url [$contentWebUrl]. " +
+          s"Response from Facebook: [${response.code}] [${response.body.string}]. " +
+          s"Was successful: [$wasSuccessful]")
+
+      } catch {
+        case e: IOException =>
+          println("Facebook call threw IOException; this could indicate a timeout: " + e.getMessage)
+          false
+        case e: Throwable =>
+          println("Facebook call threw unexpected Exception: " + e.getMessage)
           false
       }
-
-      println(s"Sent Facebook Newstab ping request for content with url [$contentWebUrl]. " +
-        s"Response from Facebook: [${response.code}] [${response.body.string}]. " +
-        s"Was successful: [$wasSuccessful]")
 
       true // Always return true during the proof on concept until we are confident about Facebook's responses
 
